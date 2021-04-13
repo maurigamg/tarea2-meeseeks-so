6a6e5a09d236b268daecd1680b310c3c8f17e236
Mauricio Gamboa <maurigamg@gmail.com>
2021-04-13 11:26:13 -0600

    -- main added
    -- main function is a menu for the users

--- SO - T2 - MauricioGamboaGodínez - JoséDanielMacíasReynaud.c
+++ SO - T2 - MauricioGamboaGodínez - JoséDanielMacíasReynaud.c
@@ -8,3 +8,46 @@ Estudiantes:
 Profesor: Esteban Arias Méndez
 Fecha de entrega: 19 de abril, 2021, I Semestre
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+char * createMeeseeks(char * query){
+	return NULL;
+}
+
+
+
+int main(int argc, char **argv){
+	char * query;
+	char continueExecution = 'S';
+	char queryType = 'S';
+
+	while(1){
+		printf("\nConsulta textual (T)\n");
+		printf("Operacion aritmetica (A)\n");
+		printf("Operacion logica (L)\n");
+		printf("Ejecutar programa (P)\n");
+		printf("Indique el tipo de solicitud: ");
+    	scanf("%c", &queryType);
+		getchar();
+
+		printf("\nRealice la solicitud: ");
+    	scanf("%s", query);
+		getchar();
+
+		printf("\nReporte\n%s\n", createMeeseeks(query));
+
+		printf("\nDesea realizar otra solicitud? (S/N): ");
+    	scanf("%c", &continueExecution);
+		getchar();
+
+		if(continueExecution == 'N' || continueExecution == 'n'){
+			break;
+		}
+	}
+	return 0;
+}
