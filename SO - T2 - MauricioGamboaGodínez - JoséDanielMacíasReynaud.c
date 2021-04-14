2e07e9afe7626a05a96557c238e31c07bb437a05
Daniel Macías <josedanielmr@gmail.com>
2021-04-14 02:58:03 -0600

    added forking and query passing and child suicide

--- SO - T2 - MauricioGamboaGodínez - JoséDanielMacíasReynaud.c
+++ SO - T2 - MauricioGamboaGodínez - JoséDanielMacíasReynaud.c
@@ -14,38 +14,86 @@ Fecha de entrega: 19 de abril, 2021, I Semestre
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
-char * createMeeseeks(char * query){
-	return NULL;
+pid_t create_meeseeks()
+{
+	pid_t pid;
+
+	pid = fork();
+	return pid;
 }
 
+char *execute_query(char *query, char query_type)
+{
+	switch (query_type)
+	{
+	case 'T':;
+		return "Soy un texto";
+		break;
+	case 'A':;
+		return "Soy un algoritmo";
+		break;
+	case 'L':;
+		return "Soy un logico";
+		break;
+	case 'P':;
+		pid_t meeseeks = create_meeseeks();
+		if (meeseeks < 0)
+		{
+			fprintf(stderr, "Mr.Meeseeks' birth failed!");
+		}
+		else if (meeseeks == 0)
+		{
+			// proceso hijo
+			printf("Mr.Meeseeks's child: pid:%d, ppid:%d! \n", getpid(), getppid());
+			int status = system(query);
+			printf("I'm done!!\n");
+			kill(getpid(), SIGKILL);
+		}
+		else
+		{
+			// proceso padre
+			printf("Proceso Padre: pid:%d, ppid:%d! \n", getpid(), getppid());
+			waitpid(meeseeks, NULL, 0);
+			printf("Hijo completado\n");
+		}
 
+		return "Soy un P";
 
-int main(int argc, char **argv){
-	char * query;
-	char continueExecution = 'S';
-	char queryType = 'S';
+		break;
+	default:;
+		return "";
+	}
+}
 
-	while(1){
+int main(int argc, char **argv)
+{
+	char *query;
+	char continue_exec = 'S';
+	char query_type = 'S';
+
+	while (1)
+	{
 		printf("\nConsulta textual (T)\n");
 		printf("Operacion aritmetica (A)\n");
 		printf("Operacion logica (L)\n");
 		printf("Ejecutar programa (P)\n");
 		printf("Indique el tipo de solicitud: ");
-    	scanf("%c", &queryType);
+		scanf("%c", &query_type);
 		getchar();
 
 		printf("\nRealice la solicitud: ");
-    	scanf("%s", query);
-		getchar();
+		scanf("%[^\n]%*c", query); //geeksforgeeks.org/taking-string-input-space-c-3-different-methods/
 
-		printf("\nReporte\n%s\n", createMeeseeks(query));
+		printf("\nReporte\n%s\n", execute_query(query, query_type));
 
 		printf("\nDesea realizar otra solicitud? (S/N): ");
-    	scanf("%c", &continueExecution);
+		scanf("%c", &continue_exec);
 		getchar();
 
-		if(continueExecution == 'N' || continueExecution == 'n'){
+		if (continue_exec == 'N' || continue_exec == 'n')
+		{
 			break;
 		}
 	}
