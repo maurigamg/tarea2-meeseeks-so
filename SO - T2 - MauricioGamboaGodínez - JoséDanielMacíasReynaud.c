22bc3ad7cddd6fb7a9ae797b00bce81619768872
Mauricio Gamboa <maurigamg@gmail.com>
2021-04-15 14:37:07 -0600

    inter process communication
    -- pipe use example

--- SO - T2 - MauricioGamboaGodínez - JoséDanielMacíasReynaud.c
+++ SO - T2 - MauricioGamboaGodínez - JoséDanielMacíasReynaud.c
@@ -16,86 +16,77 @@ Fecha de entrega: 19 de abril, 2021, I Semestre
 #include <sys/wait.h>
 #include <signal.h>
 
-pid_t create_meeseeks()
-{
+pid_t create_meeseeks(){
 	pid_t pid;
 
 	pid = fork();
 	return pid;
 }
 
-char *execute_query(char *query, char query_type)
-{
-	switch (query_type)
-	{
-	case 'T':;
-		return "Soy un texto";
-		break;
-	case 'A':;
-		return "Soy un algoritmo";
-		break;
-	case 'L':;
-		return "Soy un logico";
-		break;
-	case 'P':;
-		pid_t meeseeks = create_meeseeks();
-		if (meeseeks < 0)
-		{
-			fprintf(stderr, "Mr.Meeseeks' birth failed!");
-		}
-		else if (meeseeks == 0)
-		{
-			// proceso hijo
-			printf("Mr.Meeseeks's child: pid:%d, ppid:%d! \n", getpid(), getppid());
-			int status = system(query);
-			printf("I'm done!!\n");
-			kill(getpid(), SIGKILL);
-		}
-		else
-		{
-			// proceso padre
-			printf("Proceso Padre: pid:%d, ppid:%d! \n", getpid(), getppid());
-			waitpid(meeseeks, NULL, 0);
-			printf("Hijo completado\n");
-		}
+char * execute_query(char *query, char query_type){
+	switch (query_type){
+		case 'T':;
+			return "Soy un texto";
+			break;
+		case 'A':;
+			return "Soy un algoritmo";
+			break;
+		case 'L':;
+			return "Soy un logico";
+			break;
+		case 'P':;
+			pid_t meeseeks = create_meeseeks();
+			if (meeseeks < 0){
+				fprintf(stderr, "Mr.Meeseeks birth failed!");
+			}else if (meeseeks == 0){
+				// proceso hijo
+				printf("Mr.Meeseeks's child: pid:%d, ppid:%d! \n", getpid(), getppid());
+				int status = system(query);
+				printf("I'm done!!\n");
+				kill(getpid(), SIGKILL);
+			}else{
+				// proceso padre
+				printf("Proceso Padre: pid:%d, ppid:%d! \n", getpid(), getppid());
+				waitpid(meeseeks, NULL, 0);
+				printf("Hijo completado\n");
+			}
 
-		return "Soy un P";
+			return "Soy un P";
 
-		break;
-	default:;
-		return "";
+			break;
+		default:;
+			return "";
 	}
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
 	char *query;
 	char continue_exec = 'S';
 	char query_type = 'S';
 
-	while (1)
-	{
-		printf("\nConsulta textual (T)\n");
-		printf("Operacion aritmetica (A)\n");
-		printf("Operacion logica (L)\n");
-		printf("Ejecutar programa (P)\n");
-		printf("Indique el tipo de solicitud: ");
-		scanf("%c", &query_type);
-		getchar();
+	int a = atoi("2 + 2");
+	printf("%d", a);
+	// while (1){
+	// 	printf("\nConsulta textual (T)\n");
+	// 	printf("Operacion aritmetica (A)\n");
+	// 	printf("Operacion logica (L)\n");
+	// 	printf("Ejecutar programa (P)\n");
+	// 	printf("Indique el tipo de solicitud: ");
+	// 	scanf("%c", &query_type);
+	// 	getchar();
 
-		printf("\nRealice la solicitud: ");
-		scanf("%[^\n]%*c", query); //geeksforgeeks.org/taking-string-input-space-c-3-different-methods/
+	// 	printf("\nRealice la solicitud: ");
+	// 	scanf("%[^\n]%*c", query); //geeksforgeeks.org/taking-string-input-space-c-3-different-methods/
 
-		printf("\nReporte\n%s\n", execute_query(query, query_type));
+	// 	printf("\nReporte\n%s\n", execute_query(query, query_type));
 
-		printf("\nDesea realizar otra solicitud? (S/N): ");
-		scanf("%c", &continue_exec);
-		getchar();
+	// 	printf("\nDesea realizar otra solicitud? (S/N): ");
+	// 	scanf("%c", &continue_exec);
+	// 	getchar();
 
-		if (continue_exec == 'N' || continue_exec == 'n')
-		{
-			break;
-		}
-	}
+	// 	if (continue_exec == 'N' || continue_exec == 'n'){
+	// 		break;
+	// 	}
+	// }
 	return 0;
 }
---
+++ pipe.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main(){
+    int data_processed, data_processed2;
+    int file_pipes[2];
+    const char some_data[] = "123";
+    char buffer[BUFSIZ + 1];
+    pid_t fork_result;
+    memset(buffer, '\0', sizeof(buffer));
+    if (pipe(file_pipes) == 0) {
+        fork_result = fork();
+        if (fork_result == -1) {
+            fprintf(stderr, "Fork failure");
+            exit(EXIT_FAILURE);
+        }if (fork_result == 0) {
+            data_processed = read(file_pipes[0], buffer, BUFSIZ);
+            printf("Read %d bytes: %s PID %d\n", data_processed, buffer,getpid());
+            data_processed2 = write(file_pipes[1], "some_data",strlen(" "));
+            printf("Wrote %d bytes PID %d\n", data_processed2, getpid());
+            exit(EXIT_SUCCESS);
+        }else {
+            data_processed = write(file_pipes[1], some_data,strlen(some_data));
+            printf("Wrote %d bytes PID %d\n", data_processed, getpid());
+            data_processed2 = read(file_pipes[0], buffer, BUFSIZ);
+            printf("Read %d bytes: %s PID %d\n", data_processed2, buffer, getpid());
+        }
+    }
+    exit(EXIT_SUCCESS);
+}
