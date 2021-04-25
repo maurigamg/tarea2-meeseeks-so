3ca84f55feff7a49c64cbd36fc2d29868158e8bb
Mauricio Gamboa <maurigamg@gmail.com>
2021-04-24 21:18:58 -0600

    Encabezado

--- simulation_3.c
+++ simulation_3.c
@@ -1,3 +1,14 @@
+/*
+ITCR, Sede Cartago
+Escuela de Computación, IC-6600 Principios de Sistemas Operativos
+SO - Tarea 2 - Mr. Meeseeks
+Estudiantes:
+	Mauricio Gamboa Godínez 2018113173
+	José Daniel Macías Reynaud 2018241572
+Profesor: Esteban Arias Méndez
+Fecha de entrega: 19 de abril, 2021, I Semestre
+*/
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
